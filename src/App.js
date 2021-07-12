3460b43a0967edf5f3c682eeae6a7d7a16d6edcd
Seyed Mahdi Jalali <seyedmahdijalali2020@gmail.com>
2021-07-12 19:26:39 +0430

    Functionality added.

--- src/App.js
+++ src/App.js
@@ -1,53 +1,206 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 
 function App() {
+    const [result, setResult] = useState("");
+    const [operator, setOperator] = useState("");
+    let data;
+
+    const calculate = () => {
+        if (operator === "div") {
+            data = result.split("÷");
+            setResult(data[0] / data[1]);
+        } else if (operator === "mul") {
+            data = result.split("×");
+            setResult(data[0] * data[1]);
+        } else if (operator === "sub") {
+            data = result.split("-");
+            setResult(data[0] - data[1]);
+        } else if (operator === "add") {
+            data = result.split("+");
+            setResult(Number(data[0]) + Number(data[1]));
+        }
+        setOperator("");
+    };
+
+    const handleOperators = (optr) => {
+        if (optr === "div") {
+            operator
+                ? setResult(replaceAt(result, result.length - 1, "÷"))
+                : setResult(result + "÷");
+        } else if (optr === "mul") {
+            operator
+                ? setResult(replaceAt(result, result.length - 1, "×"))
+                : setResult(result + "×");
+        } else if (optr === "sub") {
+            operator
+                ? setResult(replaceAt(result, result.length - 1, "-"))
+                : setResult(result + "-");
+        } else if (optr === "add") {
+            operator
+                ? setResult(replaceAt(result, result.length - 1, "+"))
+                : setResult(result + "+");
+        }
+        setOperator(optr);
+    };
+
+    const replaceAt = (str, index, replacement) => {
+        return (
+            str.substr(0, index) +
+            replacement +
+            str.substr(index + replacement.length)
+        );
+    };
+
     return (
-        <div className="app">
-            <div className="container">
-                <main className="content">
-                    <header className="input-container">
-                        <input type="number" className="input" />
-                    </header>
+        <div className="container">
+            <main className="content">
+                <header className="input-container">
+                    <input
+                        type="text"
+                        className="input"
+                        value={result}
+                        onChange={(e) => setResult(e.target.value)}
+                    />
+                </header>
 
-                    <div className="keyboard">
-                        <div className="row">
-                            <button className="btn btn-clear">C</button>
-                            <button className="btn btn-colored">()</button>
-                            <button className="btn btn-colored">%</button>
-                            <button className="btn btn-colored">÷</button>
-                        </div>
-
-                        <div className="row">
-                            <button className="btn">7</button>
-                            <button className="btn">8</button>
-                            <button className="btn">9</button>
-                            <button className="btn btn-colored">×</button>
-                        </div>
-
-                        <div className="row">
-                            <button className="btn">4</button>
-                            <button className="btn">5</button>
-                            <button className="btn">6</button>
-                            <button className="btn btn-colored">-</button>
-                        </div>
-
-                        <div className="row">
-                            <button className="btn">1</button>
-                            <button className="btn">2</button>
-                            <button className="btn">3</button>
-                            <button className="btn btn-colored">+</button>
-                        </div>
-
-                        <div className="row">
-                            <button className="btn">+/-</button>
-                            <button className="btn">0</button>
-                            <button className="btn">.</button>
-                            <button className="btn btn-fill">=</button>
-                        </div>
+                <div className="keyboard">
+                    <div className="row">
+                        <button
+                            className="btn btn-clear"
+                            onClick={() => {
+                                setResult("");
+                                setOperator("");
+                            }}
+                        >
+                            C
+                        </button>
+                        <button className="btn btn-colored">()</button>
+                        <button
+                            className="btn btn-colored"
+                            onClick={() => setResult(result + "%")}
+                        >
+                            %
+                        </button>
+                        <button
+                            className="btn btn-colored"
+                            onClick={() => handleOperators("div")}
+                        >
+                            ÷
+                        </button>
                     </div>
-                </main>
-            </div>
+
+                    <div className="row">
+                        <button
+                            className="btn"
+                            onClick={() => setResult(result + 7)}
+                        >
+                            7
+                        </button>
+                        <button
+                            className="btn"
+                            onClick={() => setResult(result + 8)}
+                        >
+                            8
+                        </button>
+                        <button
+                            className="btn"
+                            onClick={() => setResult(result + 9)}
+                        >
+                            9
+                        </button>
+                        <button
+                            className="btn btn-colored"
+                            onClick={() => handleOperators("mul")}
+                        >
+                            ×
+                        </button>
+                    </div>
+
+                    <div className="row">
+                        <button
+                            className="btn"
+                            onClick={() => setResult(result + 4)}
+                        >
+                            4
+                        </button>
+                        <button
+                            className="btn"
+                            onClick={() => setResult(result + 5)}
+                        >
+                            5
+                        </button>
+                        <button
+                            className="btn"
+                            onClick={() => setResult(result + 6)}
+                        >
+                            6
+                        </button>
+                        <button
+                            className="btn btn-colored"
+                            onClick={() => handleOperators("sub")}
+                        >
+                            -
+                        </button>
+                    </div>
+
+                    <div className="row">
+                        <button
+                            className="btn"
+                            onClick={() => setResult(result + 1)}
+                        >
+                            1
+                        </button>
+                        <button
+                            className="btn"
+                            onClick={() => setResult(result + 2)}
+                        >
+                            2
+                        </button>
+                        <button
+                            className="btn"
+                            onClick={() => setResult(result + 3)}
+                        >
+                            3
+                        </button>
+                        <button
+                            className="btn btn-colored"
+                            onClick={() => handleOperators("add")}
+                        >
+                            +
+                        </button>
+                    </div>
+
+                    <div className="row">
+                        <button className="btn">+/-</button>
+                        <button
+                            className="btn"
+                            onClick={() => setResult(result + 0)}
+                        >
+                            0
+                        </button>
+                        <button
+                            className="btn"
+                            onClick={() => {
+                                result[result.length - 1] === "."
+                                    ? setResult(
+                                          replaceAt(
+                                              result,
+                                              result.length - 1,
+                                              "."
+                                          )
+                                      )
+                                    : setResult(result + ".");
+                            }}
+                        >
+                            .
+                        </button>
+                        <button className="btn btn-fill" onClick={calculate}>
+                            =
+                        </button>
+                    </div>
+                </div>
+            </main>
         </div>
     );
 }
